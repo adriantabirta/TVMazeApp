822bc0dd543ff3c2e35a5847827f640229b3c9d0
Adrian Tabirta <adriantabirta@Adrians-MacBook-Pro.local>
2017-10-08 23:36:27 +0300

    first commit proj

--- TVMazeApp/WellcomeVC.swift
+++ TVMazeApp/WellcomeVC.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class WellcomeVC: UIViewController {
-
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+	
+	@IBAction func nextPage() {
+		
+	}
 }
