3d4080837a06c40e5f839eb28d13732fa790b4d6
Adrian Tabirta <adriantabirta@Adrians-MacBook-Pro.local>
2017-10-08 23:27:16 +0300

    first commit

---
+++ TVMazeApp/WellcomeVC.swift
@@ -0,0 +1,18 @@
+//
+//  WellcomeVC.swift
+//  TVMazeApp
+//
+//  Created by Adrian Tabirta on 07.10.2017.
+//  Copyright © 2017 Adrian Tabirta. All rights reserved.
+//
+
+import UIKit
+
+class WellcomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
